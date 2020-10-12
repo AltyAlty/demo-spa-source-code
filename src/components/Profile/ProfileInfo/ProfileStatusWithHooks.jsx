45e338ab95d4a4f9306f971c3e68d56127e98340
Alty <heneveraskedforthis@gmail.com>
2020-10-12 15:42:17 +0300

    chapter 85 use useEffect hook in ProfileStatusWithHooks.jsx to fix sync bug in edit mode

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status);
+
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
